7f227adb13c6508c0f0987d02da859e5ed704c58
Manasoft404 <ngudiamanasse@gmail.com>
2018-06-24 03:43:04 -0700

    first dev integration firebase

---
+++ app/src/main/java/com/manasoft/mlkit/LivePreviewActivity.java
@@ -0,0 +1,13 @@
+package com.manasoft.mlkit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LivePreviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_live_preview);
+    }
+}
